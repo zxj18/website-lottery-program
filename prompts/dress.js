bba0352f1275bdd2315d5d38fe8180168ce243a7
张宇 <11483399+zhang-yuabcdef@user.noreply.gitee.com>
2023-10-03 15:27:38 +0000

    第二次修改
    Signed-off-by: 张宇 <11483399+zhang-yuabcdef@user.noreply.gitee.com>

---
+++ prompts/dress.js
@@ -0,0 +1,28 @@
+
+
+var app4 = new Vue({
+    el: '#app',
+    data: {
+      imgs: [
+        {prompt:"Doctor's White Coat 大夫装",aspect:"4:3"},
+        {prompt:"Sailor Uniform",aspect:"3:4"},
+        {prompt:"Flight Attendant Uniform 空姐",aspect:"3:4"},
+        {prompt:"Maid outfit 女仆装",aspect:"3:4"},
+        {prompt:"Construction Worker's Safety Vest 工装",aspect:"3:4"},
+        {prompt:"Equestrian Riding Outfit",aspect:"4:3"},
+        {prompt:"White Tailored Suit",aspect:"3:4"},
+        {prompt:"Yoga Attire",aspect:"4:3"},
+        {prompt:"Skiiing Attire",aspect:"4:3"},
+        {prompt:"Soccer Jersey",aspect:"4:3"},
+        {prompt:"Cycling Jersey and Shorts",aspect:"4:3"},
+        {prompt:"Tennis Dress",aspect:"4:3"},
+        {prompt:"White Nurse's Scrubs with trousers",aspect:"3:4"},
+        {prompt:"Golf Polo shirt and pants",aspect:"4:3"},
+        {prompt:"hanfu",aspect:"3:4"},
+        {prompt:"Tang suit",aspect:"3:4"},
+        {prompt:"cheongsam",aspect:"3:4"},
+        {prompt:"Ripped denim",aspect:"3:4"},
+        {prompt:"Uniform",aspect:"3:4"}
+      ]
+    }
+  })
